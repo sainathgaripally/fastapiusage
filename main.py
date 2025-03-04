39e79b8e49e2d21af19d082c707bf6805fa7fa89
sainathgaripally <97355929+sainathgaripally@users.noreply.github.com>
2025-03-04 09:31:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def home():
 @app.get("/metrics")
 def get_metrics():
   metrics_data = {
-    "cpu_usage" = psutil.cpu_percent(interval=1),
-    "memory_usage" = psutil.virtual_memory().percent,
-    "disk_usage" = psutil.disk_usage("/").percent,
-    "bytes_received" = psutil.net_io_counters().bytes_recv,
-    "bytes_sent" = psutil.net_io_counters().bytes_sent,
-    "uptime" = psutil.boot_time()
+    "cpu_usage" : psutil.cpu_percent(interval:1),
+    "memory_usage" : psutil.virtual_memory().percent,
+    "disk_usage" : psutil.disk_usage("/").percent,
+    "bytes_received" : psutil.net_io_counters().bytes_recv,
+    "bytes_sent" : psutil.net_io_counters().bytes_sent,
+    "uptime" : psutil.boot_time()
   }
   return metrics_data
 
